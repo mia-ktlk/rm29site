434e7a28a6e76323acac22d83e658e301d961f12
miakotalik <mia@rmtwentynine.com>
2020-08-07 02:04:46 -0700

    loader

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// ========================================================================= //
+//  //LOADER
+// ========================================================================= //
+
+
+// $(document).ready(function () {
+//   setTimeout(function () {
+//     $('.loader').fadeOut(1000);
+//     console.log("document loaded!");
+//   }, 0);
+// });
+
+function hideLoader() {
+  $('.loader').hide();
+}
+
+setTimeout(function () {
+  $(window).ready(hideLoader);
+  console.log("document loaded!");
+}, 2000);
+
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);
+
+// ========================================================================= //
+//  //POP OVER
+// ========================================================================= //
+
+
+var popOverSettings = {
+  trigger: 'hover',
+  container: 'body',
+  html: true,
+  selector: '[data-toggle="popover"]',
+
+}
+
+$('body').popover(popOverSettings);
